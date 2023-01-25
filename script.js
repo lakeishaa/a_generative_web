fe8bc892bff6174294ec09e1b3ae060ed828c437
lakeisha <sharl281@newschool.edu>
2023-01-25 15:32:23 -0500

    hi

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var w = window.innerWidth
+
+var h = window.innerHeight
+
+var rSize = Math.random()*(h/2)
+
+
+
+var testCircle = new Path.Circle(new Point(w/2, h/2), rSize);
+testCircle.fillColor = 'black';
+
